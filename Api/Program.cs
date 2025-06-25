6cc575ed7e5ee8c5a60312441d259b72594de303
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-25 13:32:25 +0700

    BATMAN

---
+++ Api/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
