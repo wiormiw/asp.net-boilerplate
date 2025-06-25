367f3be96ac590195de17a64479283a9b9f6d52b
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-25 17:57:48 +0700

    feat(application): add authorize attribute for rbac

---
+++ Application/Common/Security/AuthorizeAttribute.cs
@@ -0,0 +1,14 @@
+namespace Application.Common.Security;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class AuthorizeAttribute : Attribute
+{
+    public string Roles { get; set; } = string.Empty;
+    
+    // Uncomment this if you want policy based authorization.
+    // public string Policies { get; set; } = string.Empty;
+    
+    public AuthorizeAttribute()
+    {
+    }
+}
