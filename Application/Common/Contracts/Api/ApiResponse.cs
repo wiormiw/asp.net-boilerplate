9cb4583313bd2b390c1f769660ac4400744b5fde
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-25 17:51:40 +0700

    feat(application): add api response contract

---
+++ Application/Common/Contracts/Api/ApiResponse.cs
@@ -0,0 +1,19 @@
+namespace Application.Common.Contracts.Api;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; init; } = true;
+    public T Data { get; init; } = default!;
+    public List<string>? Messages { get; init; }
+
+    public static ApiResponse<T> SuccessResponse(T data, List<string>? messages = null)
+        => new() { Data = data, Messages = messages };
+}
+
+public class ApiErrorResponse
+{
+    public bool Success { get; init; } = false;
+    public int StatusCode { get; init; }
+    public string? Type { get; init; }
+    public List<string> Messages { get; init; } = new();
+}
