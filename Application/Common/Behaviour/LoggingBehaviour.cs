6f10829f73c03671357338b849844678c91941b2
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-25 17:59:42 +0700

    feat(application): add logging behaviour

---
+++ Application/Common/Behaviour/LoggingBehaviour.cs
@@ -0,0 +1,34 @@
+using Application.Common.Interfaces.Users;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviour;
+
+public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    where TRequest : notnull
+{
+    private readonly ILogger _logger;
+    private readonly IUser _user;
+    private readonly IIdentityService _identityService;
+
+    public LoggingBehaviour(ILogger logger, IUser user,
+        IIdentityService identityService)
+    {
+        _logger = logger;
+        _user = user;
+        _identityService = identityService;
+    }
+
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var requestName =  typeof(TRequest).Name;
+        var userId =  _user.Id;
+        string? userName = string.Empty;
+
+        if (!string.IsNullOrEmpty(userId))
+            userName = await _identityService.GetUserNameAsync(userId);
+        
+        _logger.LogInformation("Request: {Name} {@UserId} {@UserName} {@Request}",
+            requestName, userId, userName, request);
+    }
+}
