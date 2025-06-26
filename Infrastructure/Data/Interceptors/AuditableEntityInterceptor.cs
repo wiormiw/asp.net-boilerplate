2d048e416f570d14cc067971e620b92646f35ec6
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-26 12:49:47 +0700

    feat(infrastructure): add auditable entity interceptor.

---
+++ Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,65 @@
+using Application.Common.Interfaces.Users;
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Infrastructure.Data.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    private readonly IUser _user;
+    private readonly TimeProvider _dateTime;
+
+    public AuditableEntityInterceptor(IUser user, TimeProvider dateTime)
+    {
+        _user = user;
+        _dateTime = dateTime;
+    }
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntities(eventData.Context);
+        
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData,
+        InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntities(eventData.Context);
+        
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateEntities(DbContext?  context)
+    {
+        if (context is null) return;
+
+        foreach (var entry in context.ChangeTracker.Entries<BaseAuditableEntity>())
+        {
+            if (entry.State is EntityState.Added or EntityState.Modified || entry.HasChangedOwnedEntities())
+            {
+                var utcNow = _dateTime.GetUtcNow();
+
+                if (entry.State is EntityState.Added)
+                {
+                    entry.Entity.CreatedBy = _user.Id;
+                    entry.Entity.Created =  utcNow;
+                }
+                
+                entry.Entity.LastModifiedBy = _user.Id;
+                entry.Entity.LastModified =  utcNow;
+            }
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static bool HasChangedOwnedEntities(this EntityEntry entry) =>
+        entry.References.Any(r =>
+            r.TargetEntry is not null &&
+            r.TargetEntry.Metadata.IsOwned() &&
+            r.TargetEntry.State is EntityState.Added or EntityState.Modified);
+}
