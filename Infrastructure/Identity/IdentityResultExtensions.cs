cb3bfa18b6609565c5e75231672364581fd164fa
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-26 12:45:13 +0700

    feat(infrastructure): add identity result extensions.

---
+++ Infrastructure/Identity/IdentityResultExtensions.cs
@@ -0,0 +1,14 @@
+using Application.Common.Contracts.Api;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity;
+
+public static class IdentityResultExtensions
+{
+    public static Result ToApplicationResult(this IdentityResult result)
+    {
+        return result.Succeeded
+            ? Result.Success()
+            : Result.Failure(result.Errors.Select(e => e.Description));
+    }
+}
