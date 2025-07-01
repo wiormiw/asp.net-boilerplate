8e61dc2548a38c588f93ab15eed4bc9def82c932
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-07-01 19:02:10 +0700

    feat(application): api response factory include status code and provide factory for error response

--- Application/Common/Contracts/Api/ApiResponse.cs
+++ Application/Common/Contracts/Api/ApiResponse.cs
@@ -3,11 +3,12 @@ namespace Application.Common.Contracts.Api;
 public class ApiResponse<T>
 {
     public bool Success { get; init; } = true;
+    public int StatusCode { get; init; } = 200;
     public T Data { get; init; } = default!;
     public List<string>? Messages { get; init; }
 
-    public static ApiResponse<T> SuccessResponse(T data, List<string>? messages = null)
-        => new() { Data = data, Messages = messages };
+    public static ApiResponse<T> SuccessResponse(T data, int statusCode = 200, List<string>? messages = null)
+        => new() { StatusCode = statusCode, Data = data, Messages = messages };
 }
 
 public class ApiErrorResponse
@@ -16,4 +17,21 @@ public class ApiErrorResponse
     public int StatusCode { get; init; }
     public string? Type { get; init; }
     public List<string> Messages { get; init; } = [];
+    
+    public static ApiErrorResponse FromStatus(int statusCode, string?  type = null, params string[] messages)
+        => new() { StatusCode = statusCode, Type = type, Messages = messages.ToList() };
+
+    public static ApiErrorResponse FromValidation(int statusCode, string? type, Dictionary<string, string[]> errors)
+    {
+        var allMessages = errors
+            .SelectMany(kvp => 
+                kvp.Value.Select(msg => $"{kvp.Key}: {msg}")).ToList();
+
+        return new ApiErrorResponse
+        {
+            StatusCode = statusCode,
+            Type = type,
+            Messages = allMessages
+        };
+    }
 }
